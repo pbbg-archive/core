0ad6f45c7d37527394df7aab763b0d39c234248a
WaveHack <email@wavehack.net>
2018-12-02 01:04:14 +0100

    Add boot and register methods to service provider

--- src/CoreServiceProvider.php
+++ src/CoreServiceProvider.php
@@ -6,4 +6,17 @@ use Illuminate\Support\ServiceProvider;
 
 class CoreServiceProvider extends ServiceProvider
 {
+    /**
+     * Bootstrap the package services.
+     */
+    public function boot(): void
+    {
+    }
+
+    /**
+     * Register the package services.
+     */
+    public function register(): void
+    {
+    }
 }
